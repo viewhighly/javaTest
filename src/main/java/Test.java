82ab0396b076ceb8d76dabc393d71e1f9c454e26
gaozhan1 <gaozhan1@lenovo.com>
2013-12-29 21:51:29 +0800

    add Test.java

---
+++ src/main/java/Test.java
@@ -0,0 +1,12 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: gaozhan1
+ * Date: 13-12-29
+ * Time: 下午9:49
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+    public void main(String[] args) {
+        System.out.println("hello");
+    }
+}
